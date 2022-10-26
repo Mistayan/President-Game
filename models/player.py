c30667d59be42906e0a731087f0a5037ab6cf8b6
Mistayan <helixmastaz@gmail.com>
2022-10-26 09:42:09 +0200

    * renamed Player.cards to Player.hand * added is_folded(), has_folded() to know if Player still can play. * remove_from_hand() now returns the card removed

--- models/player.py
+++ models/player.py
@@ -4,22 +4,37 @@ import names
 
 class Player:
     name: str
-    cards: list[Card]
+    hand: list[Card]
+    folded: bool
 
     def __init__(self, name=None):
         self.name = name or names.get_first_name()
-        self.cards = []
+        self.hand = []
+        self.folded = False
 
-    def add_to_hand(self, card: Card):
+    def add_to_hand(self, card: Card) -> None:
+        """ add the given Card to player's hand"""
         if not isinstance(card, Card):
             raise ValueError("card must be an instance of Card.")
-        self.cards.append(card)
+        self.hand.append(card)
 
-    def remove_from_hand(self, card: Card|list[Card]):
-        if isinstance(card, list):
-            [self.remove_from_hand(card) for card in self.cards]
-            return  # End of recursive style
+    def remove_from_hand(self, card: Card) -> Card:
+        """
+        remove a specified card form player's hand.
+
+        :param card: the card to remove from player's hand
+        :return: the card removed from player's hand
+        """
         if not isinstance(card, Card):
             raise ValueError("card must be an instance of Card.")
-        self.cards.remove(card)
+        self.hand.remove(card)
+        return card
 
+    def is_folded(self):
+        return self.folded
+
+    def has_folded(self):
+        self.folded = True
+
+    def __str__(self):
+        return f"{self.name}"
