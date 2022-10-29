6936c0a3d171c331b32530cb25bbf608d36a60d9
Mistayan <helixmastaz@gmail.com>
2022-10-29 16:52:32 +0200

    Exchanges
    __player__ can give a __card__ to __someone__(game or player)

---
+++ models/utils/exchanges.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from models import root_logger
+
+
+def player_give_card_to(player, give, to):
+    try:
+        receive = player.remove_from_hand(give)
+        try:  # first, try as if player give to player
+            to.add_to_hand(receive)
+        except:  # If not a Player (method does not exist), try as if player put on game's pile
+            to.add_to_pile(receive)
+    except Exception as e:
+        root_logger.critical(e)
+        raise
