cbb2fca5c12d6c9a598bfee3c74e508f3dadfb2a
Mistayan <helixmastaz@gmail.com>
2022-11-01 22:20:22 +0100

    player now have a rank that can be set
    renamed some attributes to prevent external manipulations

--- models/rankings.py
+++ models/rankings.py
@@ -12,21 +12,27 @@ from models.player import Player
 
 
 class PresidentRank:
-    possible_rank: Final = ["President", "Vice-President", "Neutre", "Vice-Trouduc", "Trouduc"]
+    __possible_rank: Final = ["President", "Vice-President",  # positive advantages
+                              "Neutre",  # Neutral advantages
+                              "Vice-Trouffion", "Trouffion"]  # negative advantages
+
+    __advantages: Final = [2, 1, 0, -1, -2]
     rank: int
     rank_name: str
+    players = []  # Classes Instances shared attribute
 
-    def __init__(self, n, players):
+    def __init__(self, n, players: list[Player]):
         """ Ranks classifications for President Game """
         self.rank = n
-        _max = len(players)
-        current_player = players[n - 1]
+        self.players = players if not self.players else self.players
+        _max = len(self.players)
+        self.current_player = players[n - 1]
         if _max < 3 or _max > 6:
             raise CheaterDetected()
 
         # 'Neutre' can only be a valid state whenever there is an odd number of players
         # Or when there is 6 players
-        neutral = _max % 2 and n in (2, 3, 4)
+        neutral = (_max % 2 or _max == 6) and n in (2, 3, 4)
         # President is always the first player
         president = n == 1
         # Vice-president  and Vice-trouduc only exists if _max > 3
@@ -36,17 +42,26 @@ class PresidentRank:
         trouduc = n == _max
 
         if neutral:
-            self.rank_name = self.possible_rank[2]
+            self.rank_name = self.__possible_rank[2]
         if president or vice_president:
-            self.rank_name = self.possible_rank[n - 1]
+            self.rank_name = self.__possible_rank[n - 1]
         if vice_trouduc or trouduc:
-            self.rank_name = self.possible_rank[::-1][_max - n]
+            self.rank_name = self.__possible_rank[::-1][_max - n]
         try:
             self.rank_name
-        except:
+        except AttributeError:  # Attribute not set means no possible rank found
             self.rank_name = "CHEATER"
             raise CheaterDetected("No such Rank")
-        current_player.rank = self
+        print(f"{self.current_player}'s rank -> {self}")
+        self.current_player.set_rank(self)  # Give pointer
+
+    @property
+    def advantage(self) -> int:
+        """
+        :return: the current rank's advantage as number_of_cards: int
+        """
+        index = self.__possible_rank.index(self.rank_name)
+        return self.__advantages[index]
 
     def __str__(self):
         return self.rank_name
