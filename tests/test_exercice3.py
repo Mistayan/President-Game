53b0481fb3a1c9f9f69bbf1f77ca040a4c2c4f8d
Mistayan <helixmastaz@gmail.com>
2022-11-01 15:34:46 +0100

    Test winners and Ranks

--- tests/test_exercice3.py
+++ tests/test_exercice3.py
@@ -1,5 +1,34 @@
 import unittest
 
+from models import PresidentGame
 
-class TestCardsExercice3(unittest.TestCase):
-    ...
+
+class TestGameExercice3(unittest.TestCase):
+
+    def test_winner_ladder_no_neutrals(self):
+        game = PresidentGame(2, 2)
+        p1 = game.players[0]
+        p2 = game.players[1]
+        p3 = game.players[2]
+        p4 = game.players[3]
+        game.set_win(p1)
+        game.set_win(p2)
+        game.set_win(p3)
+        game.set_win(p4)
+        ladder = [winner for winner in game.winners()]
+        self.assertNotRegex(str(ladder), "Neutre")
+
+    def test_winner_ladder_two_neutrals(self):
+        game = PresidentGame(2, 3)
+        p1 = game.players[0]
+        p2 = game.players[1]
+        p3 = game.players[2]
+        p4 = game.players[3]
+        p5 = game.players[4]
+        game.set_win(p1)
+        game.set_win(p2)
+        game.set_win(p3)
+        game.set_win(p4)
+        game.set_win(p5)
+        ladder = [winner for winner in game.winners()]
+        print(ladder)
