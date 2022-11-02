0a7bfe58fcc3a9110ac86db8f1b79aed3546b2df
Mistayan <helixmastaz@gmail.com>
2022-11-02 18:41:08 +0100

    Game settings to 1 player, 3 AIs, player_name="Ready Player 1"

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     # INIT
-    game = PresidentGame(number_of_players=3, number_of_ai=0)
+    game = PresidentGame(1, 3, "Ready Player 1")
     game.start()
