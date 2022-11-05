959f5f3ee5465925451e61e89b19dd1b0769b8c0
Mistayan <helixmastaz@gmail.com>
2022-11-05 10:35:48 +0100

    reflecting tests for AIs playing full games, to assert that most functionalities do not crash the game (or infinite loop)

--- tests/test_exercice3.py
+++ tests/test_exercice3.py
@@ -12,13 +12,14 @@ class TestGameExercice3(unittest.TestCase):
             game.set_win(player)
             game._run = False
 
-        ladder = [winner for winner in game.winners()]
+        ladder = game.winners()
         # scanning for "round -> rank"
-        self.assertRegex(str(ladder), "1 -> Pres")
-        self.assertRegex(str(ladder), "2 -> Vice-P")
-        self.assertRegex(str(ladder), "3 -> Vice-T")
-        self.assertRegex(str(ladder), "4 -> Trouf")
-        self.assertNotRegex(str(ladder), "-> Neut")
+        print(ladder)
+        self.assertRegex(str(ladder), "Pres")
+        self.assertRegex(str(ladder), "Vice-P")
+        self.assertRegex(str(ladder), "Vice-T")
+        self.assertRegex(str(ladder), "Trouf")
+        self.assertNotRegex(str(ladder), "Neut")
 
     def test_winner_ladder_two_neutrals(self):
         game = PresidentGame(4, 2)
@@ -28,12 +29,12 @@ class TestGameExercice3(unittest.TestCase):
             game._run = False
         ladder = [winner for winner in game.winners()]
         # scanning for "round -> rank"
-        self.assertRegex(str(ladder), "1 -> Pres")
-        self.assertRegex(str(ladder), "2 -> Vice-P")
-        self.assertRegex(str(ladder), "3 -> Neut")
-        self.assertRegex(str(ladder), "4 -> Neut")
-        self.assertRegex(str(ladder), "5 -> Vice-T")
-        self.assertRegex(str(ladder), "6 -> Trouf")
+        self.assertRegex(str(ladder), "Pres")
+        self.assertRegex(str(ladder), "Vice-P")
+        self.assertRegex(str(ladder), "Neut")
+        self.assertRegex(str(ladder), "Neut")
+        self.assertRegex(str(ladder), "Vice-T")
+        self.assertRegex(str(ladder), "Trouf")
 
     def test_revolution(self):
         game = PresidentGame(3, 0, "Mistayan")
@@ -42,3 +43,20 @@ class TestGameExercice3(unittest.TestCase):
         # After revolution, values should be reversed
         strongest_after = game.strongest_card
         self.assertNotEqual(strongest_before, strongest_after)
+
+    def test_one_game_3_AIs(self):
+        # the simple fact that it runs until the end is proof
+        game = PresidentGame(0, 3, skip_inputs=1)
+        game.game_loop()
+
+    def test_two_games_3_AIs_one_exchange(self):
+        # the simple fact that it runs until the end is proof
+        game = PresidentGame(0, 3, skip_inputs=2)
+        game.game_loop()
+
+    def test_three_games_3_AIs(self):
+        # the simple fact that it runs until the end is proof
+        game = PresidentGame(0, 3, skip_inputs=3)
+        game.game_loop()
+
+
