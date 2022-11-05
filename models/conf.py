f9653e39149205280f675b4b2bfff7e7d34fe44f
Mistayan <helixmastaz@gmail.com>
2022-11-05 14:54:58 +0100

    added unicode_safe COLORS to export values

--- models/conf.py
+++ models/conf.py
@@ -9,4 +9,4 @@ coloredlogs.install()
 coloredlogs.set_level(logging.CRITICAL)
 root_logger: Final = logging.getLogger("root_logger")
 VALUES: Final = [str(num) for num in range(3, 11)] + ["J", "Q", "K", "A", "2"]
-COLORS: Final = ['♡', '♦', '♤', '♣']
+COLORS: Final = {'♡': 'Heart', '♦': 'Square', '♤': 'Spade', '♣': 'Clover'}  # color: unicode_safe
