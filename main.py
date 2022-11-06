fff1694f7fd954b8dd641594e3740f732da5d836
Mistayan <helixmastaz@gmail.com>
2022-11-06 23:47:22 +0100

    made code clearer

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import logging
-
 from models.game import PresidentGame
-
-logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     # INIT
     humans_names = (input("Player Name ?"), )
     game = PresidentGame(1, 3, *humans_names)
+
+    # Start the game. (no TK so far)
     game.start()
