4e3bb749b690587ebaf28d06a9da31a5717f3b42
Mistayan <helixmastaz@gmail.com>
2022-11-08 17:40:06 +0100

    added rules sets (more to come)

--- rules.py
+++ rules.py
@@ -19,8 +19,22 @@ class GameRules:
     # Player finishing the game with the best cards instantly loose instead of winning.
     FINISH_WITH_BEST_CARD__LOOSE = False
 
-    # When the best card is played by someone, end current round
-    PLAYING_BEST_CARD_END_ROUND = False
+    # When the best card is played by someone, ends current round and starts next one
+    PLAYING_BEST_CARD_END_ROUND = True
 
     # If set to False, you can play on the next turn.
-    WAIT_NEXT_ROUND_IF_FOLD = False
+    WAIT_NEXT_ROUND_IF_FOLD = True  # 'folded' acts like a 'played' status if False
+
+    # Set of rules according to rankings
+
+    # Extreme = president-troufion
+    EXTREME_RANKS = {"give": 1,   # card
+                     "below": 4,  # players
+                     "or": 2,     # cards
+                     "above": 5,   # players
+                     }
+
+    # Medium = vice-vice
+    MEDIUM_RANKS = {"exists_above": 5,  # players
+                    "give": 1,           # card
+                    }
