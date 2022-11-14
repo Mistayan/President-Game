079b1938c586851d5b10b69856e1927e51f2a70a
Mistayan <helixmastaz@gmail.com>
2022-11-14 17:44:46 +0100

    human players, now allowed to have a name ;)

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from typing import Final
+
+import names
+
 from models.games import PresidentGame
+
 
 if __name__ == '__main__':
     # INIT
-    humans_names = (input("Player Name ?"),)
     game = PresidentGame(nb_players=1, nb_ai=3)
-
+    for i in range(len(game.players)):
+        if game.players[i].is_human:
+            game.players[i].name = input("Player Name ?") or names.get_full_name()
     # Start the game. (no TK so far)
     game.start()
