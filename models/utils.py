bf57e340d7515622b1fb1733a915469fbf74bdb1
Mistayan <helixmastaz@gmail.com>
2022-11-25 00:22:01 +0100

    validate buffer and measure performance

--- models/utils.py
+++ models/utils.py
@@ -39,3 +39,48 @@ class SerializableClass(object):
         _T_dict = {public_names[i]: public_attributes[i] for i in range(len(public_names))}
         root_logger.debug(_T_dict)
         return json.loads(json.dumps(_T_dict))
+
+
+class ValidateBuffer:
+    """
+    On method call, verify buffer content (headers, requirements, tokens, ...)
+    """
+
+    def __init__(self, message_before: str = None, message_after: str = ""):
+        self.before = message_before
+        self.after = message_after
+        self.__buffer = None
+
+    def __call__(self, f):
+        def validate(*args, **kwargs):
+            self.__buffer = kwargs
+            print(f"Buffer validation ==> {f}({args}\t{kwargs})")
+            if not (args or kwargs):
+                raise BufferError("Nothing to apply. Empty buffer")
+            response = f(*args, **kwargs)
+            self.after and root_logger.debug(f"{self.after}")
+            return response
+
+        self.before and root_logger.debug(f"{self.before}")
+        return validate
+
+
+def measure_performance(func):
+    """Measure performance of a function"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        start_time = perf_counter()
+        func(*args, **kwargs)
+        current, peak = tracemalloc.get_traced_memory()
+        finish_time = perf_counter()
+        print(f'Function: {func.__name__}')
+        print(f'Method: {func.__doc__}')
+        print(f'Memory usage:\t\t {current / 10 ** 6:.6f} MB \n'
+              f'Peak memory usage:\t {peak / 10 ** 6:.6f} MB ')
+        print(f'Time elapsed is seconds: {finish_time - start_time:.6f}')
+        print(f'{"-" * 40}')
+        tracemalloc.stop()
+
+    return wrapper
