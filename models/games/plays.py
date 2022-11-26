1ffe7e4af681093ebb3a97fe3730183a51850615
Mistayan <helixmastaz@gmail.com>
2022-11-26 13:25:05 +0100

    reworked Game-Play. final ?

--- models/games/plays.py
+++ models/games/plays.py
@@ -9,8 +9,19 @@ from abc import ABC, abstractmethod
 
 
 class GamePlay(ABC):
-    """ Base Play class for Game """
+    """ Base Play class for Games """
+
+    @abstractmethod
+    def __init__(self, *args):
+        pass
+
     @abstractmethod
     def unicode_safe(self):
         """ Every Game-Plays MUST have a unicode_safe method for json exchanges """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_unicode(*args):
+        """ Avery Game-Plays MUST have a method to unhash unicoded message """
         pass
