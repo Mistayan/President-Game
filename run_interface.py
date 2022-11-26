4e9ab4ad1403533a3c2db601d56c701fe2a12611
Mistayan <helixmastaz@gmail.com>
2022-11-26 13:32:21 +0100

    KeyboardInterrupt Handled

--- run_interface.py
+++ run_interface.py
@@ -16,8 +16,12 @@ if __name__ == '__main__':
     coloredlogs.set_level(logging.INFO)
     with Interface(Human(input("Player Name ?"))) as interface:  # With, auto-disconnect on exit
         interface.menu()  # Connect or exit
-        while interface.update().status_code == 200:  # As long as we are connected, with no errors
-            if interface.action_required is True:
-                interface.request_player_action()
-            elif interface.game_dict['running'] is False:
-                interface.menu()
+        try:
+            while interface.update().status_code == 200:  # As long as we are connected, with no errors
+                if interface.action_required is True:
+                    interface.request_player_action()
+                elif interface.game_dict['running'] is False:
+                    interface.menu()
+        except KeyboardInterrupt as e:
+            print("Shutting down Interface...")
+
