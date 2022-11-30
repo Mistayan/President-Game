b1b19410abd72fd09fb932cd0cf20dc4ff3db86e
Mistayan <helixmastaz@gmail.com>
2022-11-30 22:53:53 +0100

    Lint

--- models/games/Errors.py
+++ models/games/Errors.py
@@ -8,22 +8,28 @@ Creation-date: 11/01/22
 import logging
 from typing import Final
 
-critical: Final = "".join(["\n", "🚨" * 25, "\n"])
+CRITICAL: Final = "".join(["\n", "🚨" * 25, "\n"])
 
 
 class CheaterDetected(BaseException):
-
+    """
+    Exception Raised if a player is caught cheating
+    """
     def __init__(self, e=""):
+        super().__init__(e)
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.CRITICAL)
-        logger.critical("".join([critical, str(e), critical]))
+        logger.critical("".join([CRITICAL, str(e), CRITICAL]))
         raise self
 
 
 class PlayerNotFound(BaseException):
-
+    """
+    Exception Raised if a player is not found amongst active players
+    """
     def __init__(self, e):
+        super().__init__(e)
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.CRITICAL)
-        logger.critical("".join([critical, f"Player Not Found : {str(e)}", critical]))
+        logger.critical("".join([CRITICAL, f"Player Not Found : {str(e)}", CRITICAL]))
         raise self
