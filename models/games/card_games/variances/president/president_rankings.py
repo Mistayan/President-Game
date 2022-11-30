4dc4be47e3eed6b8f708360a89731236bd08bdda
Mistayan <helixmastaz@gmail.com>
2022-11-30 22:59:27 +0100

    Lint

--- models/games/card_games/variances/president/president_rankings.py
+++ models/games/card_games/variances/president/president_rankings.py
@@ -14,10 +14,20 @@ from rules import PresidentRules
 
 
 class PresidentRank:
-    __possible_rank: Final = [rank_name for rank_name in PresidentRules.RANKINGS]
-    __advantages: Final = [PresidentRules.RANKINGS[rank] for rank in __possible_rank]
+    """ Define the game_rank name and advantage of a player.
+    Depends on PresidentRules
 
-    def __init__(self, n, player: Player, nb_players: int):
+    How to :
+    player.game_rank = PresidentRank(
+                                player's game_rank from game (int: 1-len(players)),
+                                player,
+                                len(in-game-players)
+                                )
+    """
+    __POSSIBLE_RANKS: Final = [rank_name for rank_name in PresidentRules.RANKINGS]
+    __RANK_ADVANTAGE: Final = [PresidentRules.RANKINGS[rank] for rank in __POSSIBLE_RANKS]
+
+    def __init__(self, game_rank: int, player: Player, nb_players: int):
         """ Ranks classifications for President Game """
         self.logger = logging.getLogger(__class__.__name__)
         self.nb_players = nb_players
@@ -29,14 +39,14 @@ class PresidentRank:
         vice_exists = nb_players > PresidentRules.MEDIUM_RANKS["exists_above"]
 
         # President is always the first player, vice-president is always second
-        if n == 1 or vice_exists and n == 2:
-            self.rank_name = self.__possible_rank[n - 1]
+        if game_rank == 1 or vice_exists and game_rank == 2:
+            self.rank_name = self.__POSSIBLE_RANKS[game_rank - 1]
         # Troufion is always the last player, vice-troufion is always second last
-        elif n == nb_players or vice_exists and n == nb_players - 1:
-            self.rank_name = self.__possible_rank[::-1][nb_players - n]
+        elif game_rank == nb_players or vice_exists and game_rank == nb_players - 1:
+            self.rank_name = self.__POSSIBLE_RANKS[::-1][nb_players - game_rank]
         # Anyone else is supposed to be neutral
         else:
-            self.rank_name = self.__possible_rank[2]
+            self.rank_name = self.__POSSIBLE_RANKS[2]
         player.set_rank(self)
 
     @property
@@ -52,11 +62,13 @@ class PresidentRank:
         if adv is None:  # president / troufion -> adv = None
             rnk = PresidentRules.EXTREME_RANKS
             adv = rnk["give"] if self.nb_players > rnk["above"] else rnk["else"]
-            adv = - adv if self.rank_name == self.__possible_rank[-1] else adv
+            adv = - adv if self.rank_name == self.__POSSIBLE_RANKS[-1] else adv
         return adv
 
     def __str__(self):
+        """ return rank's nomination """
         return self.rank_name
 
     def __repr__(self):
+        """ return rank's nomination in listings"""
         return self.__str__()
