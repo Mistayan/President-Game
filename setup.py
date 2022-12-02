d97c4b78cd498fba44271e07043fb58f518c398f
Mistayan <helixmastaz@gmail.com>
2022-12-03 00:44:01 +0100

    naming convention

--- setup.py
+++ setup.py
@@ -13,19 +13,19 @@ from setuptools import setup
 
 if __name__ == '__main__':
     BASEDIR = os.path.abspath(os.getcwd())
-    _env = "venv"
-    venv_py = f"{_env}/Scripts/python"
+    _ENV = "venv"
+    venv_py = f"{_ENV}/Scripts/python"
     if not os.path.exists(os.path.join(BASEDIR, "venv")):
-        print(f"Creating virtual environment : {_env}")
-        init = Popen(f"python -m venv {_env}".split()).communicate()
+        print(f"Creating virtual environment : {_ENV}")
+        init = Popen(f"python -m venv {_ENV}".split()).communicate()
         print("Checking for new pip && wheel versions... last security version.")
         check_pip = Popen(f"{venv_py} -m pip install --upgrade pip".split()).communicate()
         check_wheel = Popen(f"{venv_py} -m pip install --upgrade wheel".split()).communicate()
-        if not os.path.exists(os.path.join(BASEDIR, _env)):
+        if not os.path.exists(os.path.join(BASEDIR, _ENV)):
             raise EnvironmentError("You must setup a virtual environment to use this method.\n"
-                                   f">>> python venv {_env}\n>>> activate\n"
+                                   f">>> python venv {_ENV}\n>>> activate\n"
                                    ">>>pip install -r requirements.txt")
-    if os.path.exists(os.path.join(BASEDIR, _env)):
+    if os.path.exists(os.path.join(BASEDIR, _ENV)):
         print("Applying requirements")
         install = Popen(f"{venv_py} -m pip install -r requirements.txt".split()).communicate()
 
