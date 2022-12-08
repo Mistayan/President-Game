27da1adf45fb011c22763e9647629553a2199bb7
Mistayan <helixmastaz@gmail.com>
2022-12-08 23:18:36 +0100

    moved static vars here

--- models/conf.py
+++ models/conf.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 import logging
 import os
+import platform
 import random
 from typing import Final
 
@@ -20,3 +21,10 @@ coloredlogs.set_level(logging.CRITICAL)
 random.SystemRandom().seed("no_AI_allowed")
 ROOT_LOGGER: Final = logging.getLogger("ROOT_LOGGER")
 BASEDIR = os.path.abspath(os.getcwd())
+ENV_NAME = "venv"
+SYSTEM = platform.system()
+if SYSTEM not in ("Windows", "POSIX"):
+    raise OSError("Unknown OS type, cannot execute. Please contact software maintainer")
+VENV_PYTHON = os.path.join(ENV_NAME,
+                           "Scripts" if SYSTEM == "Windows" else "bin",  # if not windows, POSIX
+                           "python")
