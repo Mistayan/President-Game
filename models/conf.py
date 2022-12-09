0c619df73e541f7bc8827e2b0bdecbf94f6c2a56
Mistayan <hellogan.castramere@gmail.com>
2022-12-09 11:10:26 +0100

    moved envError to conf

--- models/conf.py
+++ models/conf.py
@@ -14,8 +14,10 @@ import platform
 import random
 from typing import Final
 
+import colorama
 import coloredlogs
 
+logging._defaultFormatter = logging.Formatter(u"%(message)s")
 coloredlogs.install()
 coloredlogs.set_level(logging.CRITICAL)
 random.SystemRandom().seed("no_AI_allowed")
@@ -30,3 +32,11 @@ VENV_PATH = os.path.join(BASEDIR, ENV_NAME,
                          )
 VENV_PYTHON = os.path.join(VENV_PATH,
                            "python")
+print(os.path.join(BASEDIR, ENV_NAME))
+if not os.path.exists(os.path.join(BASEDIR, ENV_NAME)):
+    print("You MUST setup a virtual environment to use this project.\n"
+          f"{colorama.Fore.LIGHTGREEN_EX}"
+          f">>> python -m venv {ENV_NAME}\n"
+          f">>> activate\n"
+          ">>> pip install -r requirements.txt"
+          f"{colorama.Fore.RESET}")
